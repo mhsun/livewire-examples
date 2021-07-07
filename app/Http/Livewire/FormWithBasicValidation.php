6a00618451a6cbdc1db63f323e9d77bba2dc38a1
Mehedi Hassan Sunny <mehedisunny@tedfo.com>
2021-07-07 18:33:03 +0600

    Add basic contact form component

---
+++ app/Http/Livewire/FormWithBasicValidation.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Contact;
+use Livewire\Component;
+
+class FormWithBasicValidation extends Component
+{
+    /**
+     * @var string
+     */
+    public $name = "";
+
+    /**
+     * @var string
+     */
+    public $email = "";
+
+    /**
+     * @var string
+     */
+    public $phone = "";
+
+    /**
+     * @var string
+     */
+    public $message = "";
+
+    /**
+     * Validation rules
+     *
+     * @var string[]
+     */
+    protected $rules = [
+        'name'    => 'required',
+        'email'   => 'required|email',
+        'phone'   => 'required',
+        'message' => 'required',
+    ];
+
+    /**
+     * This method will be fired when user click the send message button
+     *
+     * @param void
+     * @return void
+     */
+    public function sendMessage(): void
+    {
+        $formData = $this->validate();
+
+        Contact::create($formData);
+
+        session()->flash('success', 'Thank you. We will contact you soon.');
+
+        $this->clearForm();
+    }
+
+    /**
+     * This method is responsible for clearing the form
+     *
+     * @param void
+     * @return void
+     */
+    private function clearForm()
+    {
+        $this->name = "";
+        $this->email = "";
+        $this->phone = "";
+        $this->message = "";
+    }
+
+    /**
+     * This method is responsible for rendering the view
+     */
+    public function render()
+    {
+        return view('livewire.form-with-basic-validation');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <livewire:styles/>
     <style>
         body {
             font-family: 'Nunito', sans-serif;
         }
     </style>
+    @stack('styles')
 </head>
 <body>
-
+<livewire:form-without-validation/>
 <livewire:scripts/>
+
+@stack('scripts')
 </body>
 </html>
